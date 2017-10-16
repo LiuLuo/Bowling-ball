a11cb06fa89e9f1ca8a18facdd0bce9a52bcc3e3
LiuLuo <302786268@qq.com>
2017-10-16 12:47:57 +0800

    add effect

--- Assets/Scripts/BowlingBallController.cs
+++ Assets/Scripts/BowlingBallController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BowlingBallController : MonoBehaviour {
+    public GameObject effect;
+    public GameObject player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            GameObject newEffect = (GameObject)Instantiate(effect,player.transform .position,effect.transform.rotation);
+            newEffect.transform.parent = player.transform;
             gameObject.SetActive(false);
+            Destroy(newEffect, 1.0f);
         }
       
     }
